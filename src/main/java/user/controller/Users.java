1177862e5c8941a0c1f92e99b0bde53a0faac983
Randy Salazar <randysalazar235@yahoo.com>
2019-10-03 14:31:59 -0700

    Created User controller and  object

--- src/main/java/user/controller/Users.java
+++ src/main/java/user/controller/Users.java
@@ -2,5 +2,56 @@ package user.controller;
 
 public class Users 
 {
+	private int employeeId; 
+	private String firstName; 
+	private String lastName; 
+	
+	//Constructor
+	public Users()
+	{
+		
+	}
+	
+	
+	// Setters and getters 
+	
+	public int getEmployeeId()
+	{
+		return employeeId;
+	}
+	
+	public void setEmployeeId(int employeeId)
+	{
+		this.employeeId = employeeId;
+	}
+	
+	public String getFirstName()
+	{
+		return firstName;
+	}
+	
+	public void setFirstName(String firstName) 
+	{
+		this.firstName = firstName;
+	}
+	
+	public String getLastName()
+	{
+		return lastName;
+	}
+	
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
 
+	
+	
+	@Override
+    public String toString() {
+        return "Emplyee info{" +
+                "Emplouee ID: " + employeeId +
+                ", First Name: " + firstName + '\'' +
+                ", Last Name: " + lastName+ '\'' +
+                '}';
+    }
 }
