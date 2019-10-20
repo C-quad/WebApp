d799bcc0affd2f2667b16f60214af3c533c4a09f
Randy Salazar <randysalazar235@yahoo.com>
2019-10-20 14:48:19 -0700

    Added some functions for testing

--- src/main/java/com/scheduler/WebApp/repository/EmployeeRepository.java
+++ src/main/java/com/scheduler/WebApp/repository/EmployeeRepository.java
@@ -1,6 +1,7 @@
 package com.scheduler.WebApp.repository;
 
 import java.util.List;
+import java.util.UUID;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
@@ -12,6 +13,13 @@ import com.scheduler.WebApp.model.Users;
 public interface EmployeeRepository extends MongoRepository<Users, String> {
 
     public Users findByFirstName(String firstName);
+    
     public List<Users> findByLastName(String lastName);
-
+    
+  //  int insertNewEmployee(UUID employeeID, Users employee ); 
+    
+  //  int deleteEmployee(UUID employeeID);
+    
+    
+    
     }
