0911883a9cfa35525a4e602224b7502c7e6469e1
Steven <stevenphunghuynh@yahoo.com>
2019-10-20 13:40:29 -0700

    Updated home, about-us pages, added input functionality
    Added information to our home and about us pages, and input-test is able to pass JS variables to MVC controller

--- src/main/java/com/scheduler/WebApp/controller/WebController.java
+++ src/main/java/com/scheduler/WebApp/controller/WebController.java
@@ -3,6 +3,8 @@ package com.scheduler.WebApp.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -24,5 +26,10 @@ public class WebController
         // with the URL: http://localhost:8080/
         return "OK";
     }
-	
+    
+    //Gets first name / last name input from test-input page
+    @RequestMapping(value = "/name", method = RequestMethod.POST)
+    public void getNames(@RequestParam(value = "id") String firstName, @RequestParam(value = "id2") String lastName) {
+		System.out.println("First Name: " + firstName + " Last Name: " + lastName);
+    }
 }
