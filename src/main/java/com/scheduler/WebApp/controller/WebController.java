b6811f565c8fe06cf27cdfdd32cfa43512e88637
Randy Salazar <randysalazar235@yahoo.com>
2019-10-23 17:20:29 -0700

    Added crud operation to webcontroller

--- src/main/java/com/scheduler/WebApp/controller/WebController.java
+++ src/main/java/com/scheduler/WebApp/controller/WebController.java
@@ -1,7 +1,13 @@
 package com.scheduler.WebApp.controller;
 
+import java.util.List;
+import java.util.UUID;
+
+import org.apache.tomcat.jni.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -9,40 +15,59 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.scheduler.WebApp.model.Users;
+import com.scheduler.WebApp.service.EmployeeServices;
 
 
 @RestController
 public class WebController 
 {
+	@Autowired
+	private EmployeeServices employeeServices; 
 	
-    /**
-     * This is a simple example of how the HTTP API works.
-     * It returns a String "OK" in the HTTP response.
-     * To try it, run the web application locally,
-     * in your web browser, type the link:
-     *  http://localhost:8080/cs480/ping
-     */
-    @RequestMapping(value = "/cs480/ping", method = RequestMethod.GET)
-    String healthCheck() {
-        // You can replace this with other string,
-        // and run the application locally to check your changes
-        // with the URL: http://localhost:8080/
-        return "OK";
-    }
-    
-
-    
-    @RequestMapping( value ="/data", 
-    				method = RequestMethod.POST, 
-    				produces = MediaType.APPLICATION_JSON_VALUE)
-    public void insertNewEmployee(Users newEmployee)
+	
+	
+	// Add an employee to the database
+    @RequestMapping( value = "/addEmployee", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE )
+    public String insertNewEmployee(@RequestBody Users employee)
     {
+    	employeeServices.addEmployee(employee);
     	
-    }
-    	
-    	
+    	return employee.toString();
+    }	
     
+    
+    //Gets all employees from the database
+    @RequestMapping(value = "/displayAllEmployees", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Users> getAllEmployees()
+    {
+    	return employeeServices.getAllEmployees();
+    }
+    
+    /*
+    
+    //update employee
+    @RequestMapping( value = "/updateEmployee", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE )
+    public String Update(Users employee)
+    {
+    	System.out.println(employee.getFirstName());
+		System.out.println(employee.getLastName());
+    	
+    	employee = employeeServices.updateEmployeeInfo(employee.getFirstName(), employee.getLastName(),employee.isManager());
+    	
+    	return employee.toString();
+    }
+    
+    */
+    
+    //Delete employee
+    @RequestMapping(value = "/deleteEmployee", method = RequestMethod.POST)
+    public String delteEmployee(@RequestParam String firstName)
+    { 
+    	employeeServices.delete(firstName);
+    	return "Deleted" + firstName; 
+    }
   
+
     //Gets first name / last name input from test-input page
     @RequestMapping(value = "/name", method = RequestMethod.POST)
     public void getNames(@RequestParam(value = "id") String firstName, @RequestParam(value = "id2") String lastName) {
