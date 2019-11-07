56e501b627f7a3f2c1ddd52391cec734f05f48cc
Randy Salazar <randysalazar235@yahoo.com>
2019-11-07 11:48:03 -0800

    add Createuser and login file

---
+++ src/main/java/com/scheduler/WebApp/controller/LoginAndRegister.java
@@ -0,0 +1,61 @@
+package com.scheduler.WebApp.controller;
+
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.scheduler.WebApp.model.CreateUser;
+import com.scheduler.WebApp.model.Users;
+import com.scheduler.WebApp.service.EmployeeServices;
+
+@RestController
+public class LoginAndRegister 
+{
+	@Autowired
+	private EmployeeServices employeeServices;
+
+	  //Gets all values (First name, last name, email, password) from registeration page
+    @RequestMapping(value = "/checkCredentials", method = RequestMethod.POST)
+    public void getCredentials(
+    						@RequestParam(value = "fname") String firstName, @RequestParam(value = "lname") String lastName,
+    						@RequestParam(value = "em") String email, @RequestParam(value = "pass") String password,
+    						@RequestParam(value = "repass") String repassword) 
+    {
+    	System.out.println("First Name: " + firstName);
+    	System.out.println("Last Name: " + lastName);
+    	System.out.println("Email: " + email);
+    	System.out.println("Pw: " + password);
+    	System.out.println("RE:Pw: " + repassword);
+    	
+    	
+    	CreateUser newEmployee = new CreateUser(firstName, lastName, email, password, repassword);
+    	
+    	//Conditional statements 
+    	
+    	//check if user already exists 
+    	
+    	// check of password are equal to each other 
+    	
+    	// send responses back to the frontend and explain type of error 
+    	
+    	employeeServices.addEmployee(new Users(UUID.randomUUID(), newEmployee.getFirstName(), newEmployee.getLastName(), false, newEmployee.getEmail(), newEmployee.getPassword()) );
+   
+    	
+    }
+    
+    
+    
+    // login page 
+    
+    
+    
+    
+    
+		
+}
---
+++ src/main/java/com/scheduler/WebApp/model/CreateUser.java
@@ -0,0 +1,105 @@
+package com.scheduler.WebApp.model;
+
+import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+public class CreateUser 
+{
+	//@AutoWired
+	//BCryptPasswordEncoder bcrypt; 
+	
+	
+	@NotNull 
+	@NotEmpty
+	private String firstName;
+	
+	@NotNull 
+	@NotEmpty
+	private String lastName;
+	
+	@NotNull 
+	@NotEmpty
+	
+	private String email; 
+	
+	@NotNull 
+	@NotEmpty
+	private String password;
+	private String checkPassword;
+	
+	
+	public CreateUser(String firstName, String lastName, String email,  String password, String checkPassword)
+	{
+
+	//	bcrypt = new BCryptPasswordEncoder();
+		
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+		this.password = password;
+		this.checkPassword = checkPassword;
+	}
+
+	
+	
+	public boolean checkPass()
+	{	
+		return true; //bcrypt.matches(password, checkPassword);
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+
+	public String getLastName() {
+		return lastName;
+	}
+
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+
+	public String getEmail() {
+		return email;
+	}
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+	public String getPassword() {
+		return password;
+	}
+
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+
+	public String getCheckPassword() {
+		return checkPassword;
+	}
+
+
+	public void setCheckPassword(String checkPassword) {
+		this.checkPassword = checkPassword;
+	}
+	
+	
+	
+	
+	
+}
